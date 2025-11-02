7f8d22f61a5c396c96278693cb5a4ee1086a192b
sam-halfpenny <sam0.5pennys@gmail.com>
2025-11-02 16:00:33 +0000

    fixed labelling issue

--- renderer/domreader/domreader.py
+++ renderer/domreader/domreader.py
@@ -136,7 +136,7 @@ def dom_read(playwright, url: str) -> tuple[list[FinderFile], dict[FinderFile, t
                     if box:
                         brokenIms=imageBreak(output_path,{'x':box['width'],'y':box['height']},{'x':16,'y':16},{'x':box['x'],'y':box['y']})
                         for brokenIm in brokenIms:
-                            brokenOutpath=image_dir / "brimage"+str(time.time())+".png"
+                            brokenOutpath=image_dir / "brimage"+str(brokenIm['pos']['x'])+'-'+str(brokenIm['pos']['y'])+".png"
                             imageio.v2.imwrite(brokenOutpath,brokenIm['im'])
                             all_text_data.append(
                                 {
