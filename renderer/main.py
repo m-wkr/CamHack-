a13f16fea4d073afb70a5940ce1e5dc63b8a2526
Micfong <micfong2@outlook.com>
2025-11-02 11:28:03 +0000

    Plotter done

--- renderer/main.py
+++ renderer/main.py
@@ -9,63 +9,20 @@ from converter.converter import CoordinateSystem
 
 def main():
     parser = ArgumentParser(description="Turns Finder to a web browser.")
-    parser.add_argument("url", type=str, help="The URL to open.")
+    parser.add_argument("--url", type=str, help="The URL to open. If plotting, this is ignored.")
+    parser.add_argument("--plot", action="store_true", help="Make a nice plot.")
     args = parser.parse_args()
 
-    # center_x, center_y = 500, 300
-    # radius = 150
-    # count = 100
+    if not args.plot and not args.url:
+        print("Please provide either --url to open a webpage or --plot to make a nice plot.")
+        return
+    
+    if args.plot:
+        from renderer.plotter.plotter import plotter_render
 
-    # graph_items = [
-    #     FinderFile(
-    #         title=f"{i+1}",
-    #         position=(
-    #             int(center_x + math.sin(3 * i + math.pi / 2) * radius),
-    #             int(center_y + math.sin(2 * i) * radius)
-    #         ),
-    #         tag="li"
-    #     )
-    #     for i in range(count)
-    # ]
-
-    # image_x, image_y = 100, 400
-
-    # image_items = [
-    #     FinderFile(
-    #         title=f"i{i+1}",
-    #         position=(
-    #             int(image_x + i % 4 * 16),
-    #             int(image_y + i // 4 * 16)
-    #         ),
-    #         tag="img",
-    #         icon_path=f"icons/{i+1}.png"
-    #     ) for i in range(16)
-    # ]
-
-    # finder_render(files=[
-    #     FinderFile(
-    #         title="Example Link",
-    #         position=(100, 100),
-    #         is_link=True,
-    #         href="https://example.com",
-    #         tag="a"
-    #     ),
-    #     FinderFile(
-    #         title="Cam Hack 2025",
-    #         position=(100, 200),
-    #         tag="h1"
-    #     ),
-    #     FinderFile(
-    #         title="Welcome to Cam Hack 2025!",
-    #         position=(250, 200),
-    #         tag="p"
-    #     ),
-    #     *graph_items,
-    #     *image_items
-    # ])
-
-    # d = CoordinateSystem(args.url, 1000, 1000) # ds.get_width, ds.get_height
-    # coords = list(d.coord_all(10))
+        items = plotter_render()
+        finder_render("Cam Hack 2025 - Plotter", items)
+        return
 
     with sync_playwright() as playwright:
         coords, title = dom_read(playwright, args.url)
